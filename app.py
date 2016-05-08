bce80ea8a61fae4ae5c24c1357a5d74522789c60
Rasmus Tibell <rasmusti@HIQSTOM095.local>
2016-05-08 23:35:06 +0200

    fixed app for remote db

--- app.py
+++ app.py
@@ -1,33 +1,46 @@
 #!/usr/bin/python
 import MySQLdb
+import json
 
-from flask import render_template
-from app import app
+from flask import Flask
+app = Flask(__name__)
+
+def get_article():
+	db = MySQLdb.connect(host="192.168.10.22",    # your host, usually localhost
+                     user="rasse",         # your username
+                     passwd="mysql",  # your password
+                     db="rasse")        # name of the data base
+
+	# you must create a Cursor object. It will let
+	#  you execute all the queries you need
+	cur = db.cursor()
+
+	# Use all the SQL you like
+	cur.execute("SELECT * FROM product")
+
+	# print all the first cell of all the rows
+	jlist = []
+	jdata = {}
+	for row in cur.fetchall():
+		jdata[row[0]] = row[1]
+		jlist.append(jdata)
+
+	db.close()
+	return json.dumps(jlist)
+
+def get_json():
+	jlist = []
+	jdata = {}
+	jdata['a'] = 'b'
+	jlist.append(jdata)
+	return json.dumps(jlist)
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}  # fake user
-    return render_template('index.html',
-                           title='Home',
-                           user=user)
+    return get_article()
 
-db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-                     user="rasse",         # your username
-                     passwd="mysql",  # your password
-                     db="rasse")        # name of the data base
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
-cur = db.cursor()
-
-# Use all the SQL you like
-cur.execute("SELECT * FROM product")
-
-# print all the first cell of all the rows
-for row in cur.fetchall():
-    print row[0]
-    print row[1]
-
-db.close()
-
+if __name__ == "__main__":
+    app.run()
