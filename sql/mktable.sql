e335c310cb3388d5b3e7167182a9b611bcd36afb
rtibell <rasmus.tibell@gmail.com>
2016-05-09 22:56:49 +0200

    added insert to create table

--- sql/mktable.sql
+++ sql/mktable.sql
@@ -3,3 +3,7 @@ create table product (
  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
          data VARCHAR(100) 
 );
+
+insert into product (data) values ('Hink');
+insert into product (data) values ('Spade');
+commit;
